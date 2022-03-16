b29d0482f589486ed7df75e7c78180db4c2252bf
SASands <snads@live.com>
2022-03-16 18:08:53 -0400

    working on #3 pt1, need to add recognize characters in string

--- problem_solving_problems_part1.py
+++ problem_solving_problems_part1.py
@@ -1,24 +1,17 @@
 
 
 # 1. Reverse a string
-
 # a. Write code that takes a string as input and returns the string reversed
-
 # b. i.e. “Hello” will be returned as “olleH”
 
 word_to_reverse = ("Hello")       #index-Returns the index of the first element with the specified value
 final_result = ''
-
-
 for index in range(len(word_to_reverse)-1, -1, -1):     #range()-(start, stop, step)  # len()- function returns the number of items in an object.
     final_result += word_to_reverse[index]
-
 print(final_result)
 
 
-
 # 2. Capitalize letter
-
 # a. Write code that takes a string as input and capitalize the first letter of each word. Words will be separated by only one space. i.e. “hello world” should be outputted as “Hello World”
 
 word_to_be_cap =("hello world!")      #.title() Converts the first character of each word to upper case
@@ -26,28 +19,33 @@ word = word_to_be_cap.title()
 print(word)
 
 
-
 # 3. Compress a string of characters
-
 # a. For example, an input of "aaabbbbbccccaacccbbbaaabbbaaa" would compress to "3a5b4c2a3c3b3a3b3a"
 
-string_to_compress= ("aaabbbbbccccaacccbbbaaabbbaaa")  
-
-
-#txt.find("") ???
 
 # Nested for loop
-
-
 # Create a variable called "counter"
 # Create a variable called "final_result" and set equal to an empty string (will build up the final result over time)
-
 # Write a for loop that loops over each letter one by one (lets say we call the index for this loop "outer_index")
-
     # Write a nested for loop that does the same thing
         # Check letter in the word at outer_index and see if it is eqaul to the letter we are looping on in the inner loop
         # If they match, increment counter
         # If they don't match, add counter and lettter value to final result and resent counter and exit inner loop
+
+counter = "amount of letters in string"
+
+
+final_result3 = ""
+
+string_to_compress= ("aaabbbbbccccaacccbbbaaabbbaaa")    #txt.find("") ???
+
+    
+for outer_index in range(len(string_to_compress)):
+    for inner_index in range(len(string_to_compress)):
+        final_result3 += string_to_compress
+        if outer_index == inner_index:
+            print(final_result3)
+            
 
 
 
