6185ea30ee143f460f9c35d9d8bb485a614acc2e
SASands <snads@live.com>
2022-03-17 12:29:06 -0400

    still working on #3

--- problem_solving_problems_part1.py
+++ problem_solving_problems_part1.py
@@ -4,47 +4,103 @@
 # a. Write code that takes a string as input and returns the string reversed
 # b. i.e. “Hello” will be returned as “olleH”
 
-word_to_reverse = ("Hello")       #index-Returns the index of the first element with the specified value
-final_result = ''
-for index in range(len(word_to_reverse)-1, -1, -1):     #range()-(start, stop, step)  # len()- function returns the number of items in an object.
-    final_result += word_to_reverse[index]
-print(final_result)
+# word_to_reverse = ("Hello")       #index-Returns the index of the first element with the specified value
+# final_result = ''
+# for index in range(len(word_to_reverse)-1, -1, -1):     #range()-(start, stop, step)  # len()- function returns the number of items in an object.
+#     final_result += word_to_reverse[index]
+# print(final_result)
 
 
 # 2. Capitalize letter
 # a. Write code that takes a string as input and capitalize the first letter of each word. Words will be separated by only one space. i.e. “hello world” should be outputted as “Hello World”
 
-word_to_be_cap =("hello world!")      #.title() Converts the first character of each word to upper case
-word = word_to_be_cap.title()
-print(word)
+# word_to_be_cap =("hello world!")      #.title() Converts the first character of each word to upper case
+# word = word_to_be_cap.title()
+# print(word)
 
 
 # 3. Compress a string of characters
 # a. For example, an input of "aaabbbbbccccaacccbbbaaabbbaaa" would compress to "3a5b4c2a3c3b3a3b3a"
 
+    #1receiving a string of characters in a function----DONE
 
-# Nested for loop
-# Create a variable called "counter"
-# Create a variable called "final_result" and set equal to an empty string (will build up the final result over time)
-# Write a for loop that loops over each letter one by one (lets say we call the index for this loop "outer_index")
-    # Write a nested for loop that does the same thing
-        # Check letter in the word at outer_index and see if it is eqaul to the letter we are looping on in the inner loop
-        # If they match, increment counter
-        # If they don't match, add counter and lettter value to final result and resent counter and exit inner loop
+    #2setup for loop to iterate of a string indexing characters with range()
+        #2a-keep track of current character
+        #2b-keep track of next character
+        #2c- keep track of the character with a counter
+        #2d- keep track of final_result string (intial value "")
+    #3-if if current character is the same as next character 
+        #3a if they are the same increase counter by one
+    #4-else they are not the same
+        #4a-stringify counter and concatenante with current character
+            #4a.1-concatenate with final result
+                #restart the counter at 1
+    #5-return final_result string
 
-counter = "amount of letters in string"
+
+
+def receive_characters():
+    string_to_compress= ("aaabbbbbccccaacccbbbaaabbbaaa")  #txt.find("") ???
+    for character in string_to_compress:
+        print (character)
+receive_characters()
+    
+def iterate_string():
+    string_to_iterate= ("aaabbbbbccccaacccbbbaaabbbaaa")
+    iterate = iter(string_to_iterate)
+    print(next(iterate))
+iterate_string()
+
+def str_indexing():
+    characters= ""
+    string_to_index=  ("aaabbbbbccccaacccbbbaaabbbaaa")
+    string_to_index.index("aaabbbbbccccaacccbbbaaabbbaaa")
+    for character in string_to_index:
+        print (character)
+str_indexing()
 
 
 
 
-def string_character_amounts():    
-    string_to_compress= ("aaabbbbbccccaacccbbbaaabbbaaa")    #txt.find("") ???
-    final_result3 = ""  
-    for outer_index in range(len(string_to_compress)):
-        for inner_index in range(len(string_to_compress)):
-            final_result3 += string_to_compress
-            if outer_index == inner_index:
-                print(final_result3)
+
+
+
+
+# def counting_characters_in_string():
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# counter = "amount of letters in string"
+
+
+
+
+# def string_character_amounts():    
+#     final_result3 = ""  
+#         for inner_index in range(len(string_to_compress)):
+#             final_result3 += string_to_compress
+#             if outer_index == inner_index:
+#                 print(final_result3)
 
 
 
